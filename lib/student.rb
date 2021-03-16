1fbc9b50d3e02afb5d130198e0d8c977c00064d3
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-16 22:23:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,46 +5,46 @@ class Student < ActiveRecord::Base
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_accessor :name, :grade
+  # attr_accessor :name, :grade
   
-  attr_reader :id
+  # attr_reader :id
   
-  def initialize(name, grade, id=nil)
-    @id = id
-    @name = name
-    @grade = grade
-  end
+  # def initialize(name, grade, id=nil)
+  #   @id = id
+  #   @name = name
+  #   @grade = grade
+  # end
   
-  def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade TEXT
-        )
-    SQL
-    DB[:conn].execute(sql) 
-  end
+  # def self.create_table
+  #   sql =  <<-SQL 
+  #     CREATE TABLE IF NOT EXISTS students (
+  #       id INTEGER PRIMARY KEY, 
+  #       name TEXT, 
+  #       grade TEXT
+  #       )
+  #   SQL
+  #   DB[:conn].execute(sql) 
+  # end
 
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql) 
-  end
+  # def self.drop_table
+  #   sql = "DROP TABLE IF EXISTS students"
+  #   DB[:conn].execute(sql) 
+  # end
   
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
+  # def save
+  #   sql = <<-SQL
+  #     INSERT INTO students (name, grade) 
+  #     VALUES (?, ?)
+  #   SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+  #   DB[:conn].execute(sql, self.name, self.grade)
     
-  end
+  # end
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-  end
+  # def self.create(name:, grade:)
+  #   student = Student.new(name, grade)
+  #   student.save
+  # end
 
   def self.all
     sql = "SELECT * FROM students" 
