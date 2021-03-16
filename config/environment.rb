8b9036e584dfd9713a66871781431a2fc9b7ed54
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-16 22:25:42 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,20 @@
 require 'sqlite3'
 require 'pry'
-require 'active_record'
+# require 'active_record'
 
 require_relative "../lib/student.rb"
-require_relative 'sql_runner'
+# require_relative 'sql_runner'
 
-DB = ActiveRecord::Base.establish_connection(
-      :adapter => "sqlite3",
-      :database => "../students"
-    )
+# DB = ActiveRecord::Base.establish_connection(
+#       :adapter => "sqlite3",
+#       :database => "../students"
+#     )
 
-  DB = ActiveRecord::Base.connection
+#   DB = ActiveRecord::Base.connection
 
-  if ENV["ACTIVE_RECORD_ENV"] == "test"
-    ActiveRecord::Migration.verbose = false
-  end
+#   if ENV["ACTIVE_RECORD_ENV"] == "test"
+#     ActiveRecord::Migration.verbose = false
+#   end
 
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
